6b12de56631b87212f8614e267981fe8ac28b91a
Chris Fernando <c.fernando@optusnet.com.au>
2021-06-07 08:45:17 +0000

    Almost done, and now using sed - but just realised there's an ansible.builtin.replace

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 variable "ssh_key_private" {
 	type = string
-	
+	default = "private.key"
 }
 
 resource "aws_instance" "jripper" {
@@ -18,8 +18,11 @@ resource "aws_instance" "jripper" {
 		
 	}
 	
+	# terraform state does not include changes to remote-exec - eg. python2 vs python3
+	# this is really just used to halt execution until the ec2 instance is ready for us to run the playbook against it
 	provisioner "remote-exec" {
-		inline = ["curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py",  "sudo python3 get-pip.py --user", "sudo python3 -m pip install --user ansible"]
+		#inline = ["curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py",  "sudo python3 get-pip.py --user", "sudo python3 -m pip install --user ansible"]
+		inline = ["ls"]
 
 		connection {
 			type        = "ssh"
@@ -29,10 +32,22 @@ resource "aws_instance" "jripper" {
 			
 		}
 	}
-
 	
+	# set the IP address of the new host
+	provisioner "local-exec" {
+		command = "sed -i 's/##PLACEHOLDER##/${self.public_ip}/g' jripper.yml" 
+		
+	}
+	
+	# run the playbook
 	provisioner "local-exec" {
 		command = "ansible-playbook -u ubuntu -i '${self.public_ip},' --private-key ${var.ssh_key_private} jripper.yml" 
+		
+	}
+	
+	# put the placeholder back for next run
+	provisioner "local-exec" {
+		command = "sed -i 's/${self.public_ip}/##PLACEHOLDER##/g' jripper.yml" 
 		
 	}
 }
